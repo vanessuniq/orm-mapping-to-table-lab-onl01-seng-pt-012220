7c4da7c6896189d9320b9eb0def81ae1d07fa3e3
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 01:24:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,10 @@
 class Student
+  
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
