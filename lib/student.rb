8012b6a66a522574d424cb87ca1632afbf52c5e2
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 01:28:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,11 @@ class Student
     @grade = grade
     @id = id
   end
+  
+  def self.create_table
+    sql = "CREATE TABLE if not EXIST students;"
+    DB[:conn].execute(sql)
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
